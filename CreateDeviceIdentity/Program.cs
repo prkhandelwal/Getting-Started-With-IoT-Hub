5fd7e00c929bc543e4c4ebdbd2982df5110e965f
Pratik Khandelwal <prkhandelwal@hotmail.com>
2017-12-06 03:21:55 +0530

    Add project files.

---
+++ CreateDeviceIdentity/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace CreateDeviceIdentity
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        static string connectionString = "{IoT  Hub Connection String Goes Here}";
+        static void Main(string[] args)
+        {
+        }
+    }
+}
